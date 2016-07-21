2b88bb0a32abb866fc2a95bb264b569069bdd406
shoarora <shoarora@stanford.edu>
2016-07-20 23:31:36 -0700

    functional

--- main.js
+++ main.js
@@ -4,11 +4,14 @@
  * @see http://developer.chrome.com/apps/app.runtime.html
  * @see http://developer.chrome.com/apps/app.window.html
  */
-if (document.title.indexOf("Facebook") != -1) {
+if (document.body.innerHTML.indexOf("Chai") != -1) {
 	//Creating Elements
 	var btn = document.createElement("BUTTON");
 	var t = document.createTextNode("CLICK ME");
 	btn.appendChild(t);
 	//Appending to DOM
 	document.body.appendChild(btn);
+    document.body.innerHTML = document.body.innerHTML.replace(/Chaitanya/g, 'Dickface');
+
+    document.body.innerHTML = document.body.innerHTML.replace(/Chai/g, 'Dickface');
 }
