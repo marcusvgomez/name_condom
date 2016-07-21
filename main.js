f4e62c1a55afd8f7ef7cc712348c0a57d1db870e
shoarora <shoarora@stanford.edu>
2016-07-20 23:39:07 -0700

    added last name

--- main.js
+++ main.js
@@ -4,14 +4,17 @@
  * @see http://developer.chrome.com/apps/app.runtime.html
  * @see http://developer.chrome.com/apps/app.window.html
  */
-if (document.body.innerHTML.indexOf("Chai") != -1) {
-	//Creating Elements
-	var btn = document.createElement("BUTTON");
-	var t = document.createTextNode("CLICK ME");
-	btn.appendChild(t);
-	//Appending to DOM
-	document.body.appendChild(btn);
-    document.body.innerHTML = document.body.innerHTML.replace(/Chaitanya/g, 'Dickface');
 
-    document.body.innerHTML = document.body.innerHTML.replace(/Chai/g, 'Dickface');
+
+if (document.body.innerHTML.indexOf("Chai") != -1) {
+    //Creating Elements
+    var btn = document.createElement("BUTTON");
+    var t = document.createTextNode("CLICK ME");
+    btn.appendChild(t);
+    //Appending to DOM
+    document.body.appendChild(btn);
+    document.body.innerHTML = document.body.innerHTML.replace(/Chaitanya/g, 'Dickface');
+    document.body.innerHTML = document.body.innerHTML.replace(/Chai/gi, 'Dickface');
+    document.body.innerHTML = document.body.innerHTML.replace(/Asawa/g, '');
+
 }
