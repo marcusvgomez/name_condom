2e003d993edea87a681646ba72277e5ad4183379
Marcus Vincent Gomez <mvgomez@stanford.edu>
2016-07-20 23:12:56 -0700

    scan

--- main.js
+++ main.js
@@ -4,20 +4,11 @@
  * @see http://developer.chrome.com/apps/app.runtime.html
  * @see http://developer.chrome.com/apps/app.window.html
  */
-chrome.app.runtime.onLaunched.addListener(function() {
-  // Center window on screen.
-  var screenWidth = screen.availWidth;
-  var screenHeight = screen.availHeight;
-  var width = 500;
-  var height = 300;
-
-  chrome.app.window.create('index.html', {
-    id: "helloWorldID",
-    outerBounds: {
-      width: width,
-      height: height,
-      left: Math.round((screenWidth-width)/2),
-      top: Math.round((screenHeight-height)/2)
-    }
-  });
-});
+if (document.title.indexOf("Google") != -1) {
+	//Creating Elements
+	var btn = document.createElement("BUTTON")
+	var t = document.createTextNode("CLICK ME");
+	btn.appendChild(t);
+	//Appending to DOM 
+	document.body.appendChild(btn);
+}
